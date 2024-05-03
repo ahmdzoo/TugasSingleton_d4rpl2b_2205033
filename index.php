0559a5ef30dc19c508687a8f197e1b4a8c42341f
ahmdzoo <106076821+ahmdzoo@users.noreply.github.com>
2024-05-04 01:43:06 +0700

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Produk</title>
-    <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -29,7 +28,6 @@
 
                 if ($products) {
                     foreach ($products as $row) {
-                        // Format harga tanpa angka nol di belakang titik desimal
                         $formattedPrice = rtrim(rtrim(number_format($row['harga'], 2, '.', ''), '0'), '.');
 
                         echo "<tr>";
@@ -47,7 +45,6 @@
         </table>
     </div>
 
-    <!-- Bootstrap JS (Optional) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
